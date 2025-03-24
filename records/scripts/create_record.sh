11a2a675a4972e21279cd5ca9769b7235bd92c98
NU1LSPAXE\nu1ls <82641653+nu1lspaxe@users.noreply.github.com>
2025-03-25 00:13:37 +0800

    fix: curl create record port

--- records/scripts/create_record.sh
+++ records/scripts/create_record.sh
@@ -1,4 +1,4 @@
-curl -X POST https://localhost:8443/v1/records \
+curl -X POST https://localhost:8444/v1/records \
   --cacert certs/cert.pem \
   -H "Content-Type: application/json" \
-  -d '{"user_id": 123, "amount": 45.67, "record_type": "expense", "transaction_date": 1677654321, "detail": "Grocery shopping"}'
+  -d '{"user_id": 1, "amount": 45.67, "record_type": "expense", "transaction_date": 1677654321, "detail": "Grocery shopping"}'
